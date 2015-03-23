5b63da1f6af0c09d2e53e2e1ce2865f58841c5c6
Leisy Moliner <lhmoliner@gmail.com>
2015-03-23 13:27:25 -0600

    Project base :)
    Hi guys! Uploading a starting structure for the project. Of course, we
    can change anything and everything... just a starting point :)

---
+++ TravelExperts/app/src/main/java/com/sait/team6/travelexperts/classes/Login.java
@@ -0,0 +1,7 @@
+package com.sait.team6.travelexperts.classes;
+
+/**
+ * Created by 677958 on 3/23/2015.
+ */
+public class Login {
+}
---
+++ TravelExperts/app/src/main/java/com/sait/team6/travelexperts/databases/CustomerDB.java
@@ -0,0 +1,7 @@
+package com.sait.team6.travelexperts.databases;
+
+/**
+ * Created by 677958 on 3/23/2015.
+ */
+public class CustomerDB {
+}
