aaf596a4552078bf27d7a71b4f479358f22138af
maggyR <mghptl@yahoo.com>
2015-03-23 20:05:35 -0600

    Revert "new with changes to gradle"
    This reverts commit c5edccea23cd441f4ae2b7c2143731fc5c733afb.

--- TravelExperts/app/src/main/java/com/sait/team6/travelexperts/ui/MainActivity.java
+++ TravelExperts/app/src/main/java/com/sait/team6/travelexperts/ui/MainActivity.java
@@ -1,16 +1,11 @@
 package com.sait.team6.travelexperts.ui;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.sait.team6.travelexperts.R;
-
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,8 +14,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ButterKnife.inject(this);
     }
 
 
@@ -44,13 +37,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @OnClick(R.id.hourlyButton)
-    public void startHourlyActivity(View view) {
-        Intent intent = new Intent(this, AgentActivity.class);
-        //intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
-        startActivity(intent);
-
     }
 }
